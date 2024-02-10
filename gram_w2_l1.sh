6e22f82a197aa0ab07090506ec1b417c79896322
Binom-O <159266722+Binom-O@users.noreply.github.com>
2024-02-10 12:59:42 +0300

    Update gram_w2_l1.sh

--- gram_w2_l1.sh
+++ gram_w2_l1.sh
@@ -15,9 +15,8 @@ git clone https://github.com/TrueCarry/JettonGramGpuMiner.git
 cd JettonGramGpuMiner
 wget https://github.com/tontechio/pow-miner-gpu/releases/download/20211230.1/minertools-cuda-ubuntu-18.04-x86-64.tar.gz
 tar -xvf minertools-cuda-ubuntu-18.04-x86-64.tar.gz
-echo "SEED=$SEED" > config.txt
-echo "TARGET_ADDRESS=$TARGET_ADDRESS" >> config.txt
-
+echo "SEED=$SEED
+TARGET_ADDRESS=$TARGET_ADDRESS" > config.txt
 
 cat <<EOF > /root/JettonGramGpuMiner/mine.sh
 #!/bin/bash
